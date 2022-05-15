b2cc7a633433f0b0fd11e14e18df61076104c9fd
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-05-15 10:11:37 +0500

    added the js file

---
+++ app.js
@@ -0,0 +1,45 @@
+const productList ={
+    products:[
+        {
+            title:'A pillow',
+            imageUrl:'./img/forest-balcony.webp',
+            price:20,
+            description:'A soft Pillow'
+        },
+        {
+            title:'a Love story',
+            imageUrl:'./img/2.jpg',
+            price:'200',
+            description:'best thisnggs in this world',
+        },
+        {
+            title:'Famouse Persons',
+            imageUrl:'./img/1.jpg',
+            price:'1000',
+            description:'the best Software Engeneer in the world ,He is the  one of rich man in our world',
+        }
+    ],
+    render(){
+        const renderHook=document.getElementById('app');
+        const prodList =document.createElement('ul');
+        prodList.className='product-list';
+        for(const prod of this.products){
+            const prodEl=document.createElement('li');
+            prodEl.className='product-item';
+            prodEl.innerHTML=`
+            <div>
+            <img src="${prod.imageUrl}" alt="${prod.title}>
+            <div class="product-item_content">
+            <h2>${prod.title}</h2>
+            <h3>\$${prod.price}</h3>
+            <p>${prod.description}</p>
+            <button>add to card</button>
+            </div>
+            </div>
+            `;
+            prodList.append(prodEl);
+        }
+        renderHook.append(prodList);
+    }
+};
+productList.render();
